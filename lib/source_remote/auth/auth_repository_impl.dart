06b727dca63b3a1db5b3ad996218121f306ac0a4
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-19 20:10:06 +0100

    Fixed method implementation

--- lib/source_remote/auth/auth_repository_impl.dart
+++ lib/source_remote/auth/auth_repository_impl.dart
@@ -2,20 +2,18 @@ import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   @override
-  Future<bool> loginUser(String email, String password) async {
-    Future.delayed(
+  Future<bool> loginUser(String email, String password) {
+    return Future.delayed(
       const Duration(seconds: 2),
       () => true,
     );
-    throw Exception('Failed to login user');
   }
 
   @override
-  Future<bool> registerUser(String email, String password) async {
-    Future.delayed(
+  Future<bool> registerUser(String email, String password) {
+    return Future.delayed(
       const Duration(seconds: 2),
       () => true,
     );
-    throw Exception('Failed to register user.');
   }
 }
