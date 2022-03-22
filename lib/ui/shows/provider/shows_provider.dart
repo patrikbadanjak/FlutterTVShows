e26cb27424d9e182bd8f7c98b78e7cf8e99585b8
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-22 22:11:21 +0100

    Added ShowsProvider

---
+++ lib/ui/shows/provider/shows_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tv_shows/common/models/models.dart';
+
+class ShowsProvider extends ChangeNotifier {
+  bool _showsHidden = false;
+  bool get showsHidden => _showsHidden;
+
+  Show? _selectedShow;
+
+  Show? get selectedShow => _selectedShow;
+
+  void toggleShowsHidden() {
+    _showsHidden = !_showsHidden;
+    notifyListeners();
+  }
+
+  void onShowClicked(Show show) {
+    _selectedShow = show;
+  }
+
+  Future<List<Show>> get shows async {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => _showsHidden ? List<Show>.empty() : Show.allShows,
+    );
+  }
+}
