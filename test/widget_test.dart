7778134462978f4621d0078b57dd90c6e2655706
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-24 21:51:10 +0100

    Fixed import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tv_shows/main.dart';
+import 'package:tv_shows/ui/tv_shows_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
