2dc4e217204ee93e39c65880d6d75f78345bc127
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-24 21:21:43 +0100

    Extracted TVShowsApp to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tv_shows/ui/login_register/screens/login_screen.dart';
-import 'package:tv_shows/ui/shows/provider/shows_provider.dart';
-import 'package:tv_shows/ui/tv_shows_theme.dart';
+
+import 'ui/tv_shows_app.dart';
 
 void main() {
   runApp(const TVShowsApp());
-}
-
-class TVShowsApp extends StatelessWidget {
-  const TVShowsApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ShowsProvider()),
-      ],
-      child: MaterialApp(
-        title: 'TV Shows',
-        theme: TVShowsTheme.light(),
-        home: const LoginScreen(),
-      ),
-    );
-  }
 }
