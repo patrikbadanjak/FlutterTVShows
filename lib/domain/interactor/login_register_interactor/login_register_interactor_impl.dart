927d4b432a573b17585883f484c678438dbc246a
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-26 20:44:27 +0100

    Changed method signatures to return User instead of bool

--- lib/domain/interactor/login_register_interactor/login_register_interactor_impl.dart
+++ lib/domain/interactor/login_register_interactor/login_register_interactor_impl.dart
@@ -1,20 +1,20 @@
+import 'package:tv_shows/common/models/user.dart';
 import 'package:tv_shows/source_remote/auth/auth_repository.dart';
-import 'package:tv_shows/source_remote/auth/auth_repository_impl.dart';
 
 import 'login_register_interactor.dart';
 
 class LoginRegisterInteractorImpl implements LoginRegisterInteractor {
-  LoginRegisterInteractorImpl() : _authRepository = AuthRepositoryImpl();
+  LoginRegisterInteractorImpl(this._authRepository);
 
   final AuthRepository _authRepository;
 
   @override
-  Future<bool> loginUser(String email, String password) async {
+  Future<User> loginUser(String email, String password) async {
     return await _authRepository.loginUser(email, password);
   }
 
   @override
-  Future<bool> registerUser(String email, String password) async {
+  Future<User> registerUser(String email, String password) async {
     return await _authRepository.registerUser(email, password);
   }
 }
