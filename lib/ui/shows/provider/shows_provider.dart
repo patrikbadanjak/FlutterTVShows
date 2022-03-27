da67d5fc2dfb11aab28eacb8cbf885a1a5480f0e
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-27 14:47:06 +0200

    Added getter for show reviews

--- lib/ui/shows/provider/shows_provider.dart
+++ lib/ui/shows/provider/shows_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:tv_shows/common/models/review.dart';
 import 'package:tv_shows/common/models/show.dart';
 import 'package:tv_shows/common/utility/state/request_provider.dart';
 import 'package:tv_shows/source_remote/shows/shows_repository.dart';
@@ -30,6 +30,17 @@ class ShowsProvider extends RequestProvider<Show> {
   Future<List<Show>?> get shows async {
     try {
       return await repository.getShows();
+    } on Exception catch (e) {
+      _errorMessage = e.toString();
+      notifyListeners();
+    }
+
+    return null;
+  }
+
+  Future<List<Review>?> get reviews async {
+    try {
+      return await repository.getReviewsForShow(_selectedShow?.id ?? -1);
     } on Exception catch (e) {
       _errorMessage = e.toString();
       notifyListeners();
