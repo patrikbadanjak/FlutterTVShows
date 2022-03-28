c9a709196d777572d1ab0a8bd9b6db4ce4398568
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-28 19:56:30 +0200

    Simplified by removing Consumer

--- lib/ui/show_details/components/show_rating.dart
+++ lib/ui/show_details/components/show_rating.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tv_shows/ui/shows/provider/shows_provider.dart';
 
 import 'rating_stars.dart';
 
 class ShowRating extends StatelessWidget {
-  const ShowRating({Key? key}) : super(key: key);
+  const ShowRating({
+    Key? key,
+    required this.averageRating,
+    required this.numOfReviews,
+  }) : super(key: key);
+
+  final double averageRating;
+  final int numOfReviews;
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ShowsProvider>(
-      builder: (context, provider, child) {
-        final show = provider.selectedShow;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '${show!.numOfReviews} review(s), ${show.averageRating} average'.toUpperCase(),
-              style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                    color: const Color(0xFF999999),
-                  ),
-            ),
-            const SizedBox(height: 8.0),
-            IconTheme(
-              data: IconThemeData(
-                color: Theme.of(context).primaryColor,
-                size: 23.0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$numOfReviews review(s), $averageRating average'.toUpperCase(),
+          style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                color: const Color(0xFF999999),
               ),
-              child: RatingStars(
-                show.averageRating,
-              ),
-            ),
-          ],
-        );
-      },
+        ),
+        const SizedBox(height: 8.0),
+        IconTheme(
+          data: IconThemeData(
+            color: Theme.of(context).primaryColor,
+            size: 23.0,
+          ),
+          child: RatingStars(
+            averageRating,
+          ),
+        ),
+      ],
     );
   }
 }
