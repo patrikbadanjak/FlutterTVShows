f98aabde98ea168c4d1efdcf6e686ee11207359d
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-29 23:55:20 +0200

    Replaced MultiProvider with single RegisterScreenProvider

--- lib/ui/login_register/screens/register_screen.dart
+++ lib/ui/login_register/screens/register_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tv_shows/domain/interactor/login_register_interactor/login_register_interactor.dart';
-import 'package:tv_shows/domain/interactor/login_register_interactor/login_register_interactor_impl.dart';
 import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 import 'package:tv_shows/ui/login_register/screens/base_login_screen.dart';
 
@@ -13,19 +11,10 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<LoginRegisterInteractor>(
-          create: (_) => LoginRegisterInteractorImpl(
-            context.read<AuthRepository>(),
-          ),
-        ),
-        ChangeNotifierProvider<RegisterScreenProvider>(
-          create: (ctx) => RegisterScreenProvider(
-            ctx.read<LoginRegisterInteractor>(),
-          ),
-        ),
-      ],
+    return ChangeNotifierProvider<RegisterScreenProvider>(
+      create: (ctx) => RegisterScreenProvider(
+        ctx.read<AuthRepository>(),
+      ),
       child: const BaseLoginScreen(form: RegisterForm()),
     );
   }
