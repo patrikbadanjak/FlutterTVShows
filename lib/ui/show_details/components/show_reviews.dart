29abe680ddb7d468cf083ec216797832dc873ccb
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-29 11:25:02 +0200

    Code styling

--- lib/ui/show_details/components/show_reviews.dart
+++ lib/ui/show_details/components/show_reviews.dart
@@ -12,57 +12,60 @@ class ShowReviews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ReviewProvider>(
-      builder: (context, provider, child) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-            'Reviews',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          const SizedBox(height: 16.0),
-          FutureBuilder(
-            future: provider.getReviewsForShow(),
-            builder: (context, AsyncSnapshot<List<Review>?> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ShowRating(averageRating: provider.show.averageRating, numOfReviews: provider.show.numOfReviews),
-                      ListView.separated(
-                        primary: false,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          final Review review = snapshot.data![index];
-                          return ReviewListItem(
-                            key: Key(review.id.toString()),
-                            review: review,
-                          );
-                        },
-                        separatorBuilder: (context, index) => const Divider(
-                          indent: 10.0,
-                          endIndent: 10.0,
+      builder: (context, provider, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Reviews',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: 16.0),
+            FutureBuilder(
+              future: provider.getReviewsForShow(),
+              builder: (context, AsyncSnapshot<List<Review>?> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ShowRating(
+                            averageRating: provider.show.averageRating, numOfReviews: provider.show.numOfReviews),
+                        ListView.separated(
+                          primary: false,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            final Review review = snapshot.data![index];
+                            return ReviewListItem(
+                              key: Key(review.id.toString()),
+                              review: review,
+                            );
+                          },
+                          separatorBuilder: (context, index) => const Divider(
+                            indent: 10.0,
+                            endIndent: 10.0,
+                          ),
+                          itemCount: snapshot.data?.length ?? 0,
                         ),
-                        itemCount: snapshot.data?.length ?? 0,
+                      ],
+                    );
+                  } else {
+                    return const SizedBox(
+                      height: 70.0,
+                      child: Center(
+                        child: Text('No reviews yet.'),
                       ),
-                    ],
-                  );
+                    );
+                  }
                 } else {
-                  return const SizedBox(
-                    height: 70.0,
-                    child: Center(
-                      child: Text('No reviews yet.'),
-                    ),
-                  );
+                  return const Center(child: CircularProgressIndicator());
                 }
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-        ],
-      ),
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
