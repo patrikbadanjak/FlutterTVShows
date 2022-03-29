9eb54ea5b5e68cf791bc702f656cbd2082ce057c
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-29 23:55:34 +0200

    Replaced MultiProvider with single LoginScreenProvider

--- lib/ui/login_register/screens/login_screen.dart
+++ lib/ui/login_register/screens/login_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tv_shows/domain/interactor/login_register_interactor/login_register_interactor.dart';
-import 'package:tv_shows/domain/interactor/login_register_interactor/login_register_interactor_impl.dart';
 import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 import 'package:tv_shows/ui/login_register/provider/login_screen_provider.dart';
 import 'package:tv_shows/ui/login_register/screens/base_login_screen.dart';
@@ -13,19 +11,10 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<LoginRegisterInteractor>(
-          create: (_) => LoginRegisterInteractorImpl(
-            context.read<AuthRepository>(),
-          ),
-        ),
-        ChangeNotifierProvider<LoginScreenProvider>(
-          create: (ctx) => LoginScreenProvider(
-            ctx.read<LoginRegisterInteractor>(),
-          ),
-        ),
-      ],
+    return ChangeNotifierProvider<LoginScreenProvider>(
+      create: (ctx) => LoginScreenProvider(
+        ctx.read<AuthRepository>(),
+      ),
       child: const BaseLoginScreen(
         form: LoginForm(),
       ),
