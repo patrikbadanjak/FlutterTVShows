61d1c1bee254995b4e8cdd5953d7a7eac7761c88
Patrik Badanjak <pbadanjak@gmail.com>
2022-03-29 23:48:47 +0200

    Removed unnecessary interactor

--- lib/domain/interactor/login_register_interactor/login_register_interactor.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tv_shows/common/models/user.dart';
-
-abstract class LoginRegisterInteractor {
-  Future<User> loginUser(String email, String password);
-  Future<User> registerUser(String email, String password);
-}
