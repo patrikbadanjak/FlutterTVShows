3a911ba84fa1c30127e1eb69edc46a9689dad033
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-02 19:02:02 +0200

    Checks if user data is present and passes user on

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'common/models/user.dart';
+import 'domain/data_holder/storage_repository.dart';
 import 'ui/tv_shows_app.dart';
 
-void main() {
-  runApp(const TVShowsApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storageRepo = StorageRepository();
+
+  await Hive.initFlutter();
+
+  User? user;
+
+  if (await storageRepo.authInfo != null && await Hive.boxExists('user')) {
+    final box = await Hive.openBox('user');
+
+    user = User(
+      box.get('id'),
+      box.get('email'),
+      box.get('imageUrl') as String?,
+    );
+  }
+
+  runApp(TVShowsApp(
+    user: user,
+  ));
 }
