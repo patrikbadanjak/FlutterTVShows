d68935e59a419bb8fa2f9f9a983c4df0462b7b69
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-02 19:03:57 +0200

    Implemented user data storing with Hive

--- lib/source_remote/auth/auth_repository_impl.dart
+++ lib/source_remote/auth/auth_repository_impl.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
 import 'package:tv_shows/common/models/user.dart';
-import 'package:tv_shows/common/utility/interceptor/auth_info_interceptor.dart';
 import 'package:tv_shows/common/utility/interceptor/error_extractor_interceptor.dart';
 import 'package:tv_shows/domain/data_holder/storage_repository.dart';
 import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 
 import '../../common/utility/auth_info.dart';
+import '../../common/utility/interceptor/auth_info_interceptor.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   AuthRepositoryImpl(StorageRepository authInfoHolder) : _authInfoHolder = authInfoHolder {
@@ -27,6 +28,13 @@ class AuthRepositoryImpl implements AuthRepository {
   final StorageRepository _authInfoHolder;
   late final Dio _dio;
 
+  Future<void> _storeUserToHive(User user) async {
+    final box = await Hive.openBox('user');
+    box.put('id', user.id);
+    box.put('email', user.email);
+    box.put('imageUrl', user.imageUrl);
+  }
+
   @override
   Future<User> loginUser(String email, String password) async {
     final response = await _dio.post(
@@ -41,7 +49,11 @@ class AuthRepositoryImpl implements AuthRepository {
 
     final json = response.data['user'];
 
-    return User.fromJson(json);
+    final User user = User.fromJson(json);
+
+    await _storeUserToHive(user);
+
+    return user;
   }
 
   @override
@@ -59,6 +71,10 @@ class AuthRepositoryImpl implements AuthRepository {
 
     final json = response.data['user'];
 
-    return User.fromJson(json);
+    final User user = User.fromJson(json);
+
+    await _storeUserToHive(user);
+
+    return user;
   }
 }
