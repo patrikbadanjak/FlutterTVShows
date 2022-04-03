2fb03d76d242efbe87100661cfe3d9f93fa3fcec
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-04 01:57:57 +0200

    Added methods for logout and user info update

--- lib/source_remote/auth/auth_repository_impl.dart
+++ lib/source_remote/auth/auth_repository_impl.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tv_shows/common/models/user.dart';
 import 'package:tv_shows/common/utility/interceptor/error_extractor_interceptor.dart';
 import 'package:tv_shows/domain/data_holder/storage_repository.dart';
@@ -8,7 +12,7 @@ import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 import '../../common/utility/auth_info.dart';
 import '../../common/utility/interceptor/auth_info_interceptor.dart';
 
-class AuthRepositoryImpl implements AuthRepository {
+class AuthRepositoryImpl with ChangeNotifier implements AuthRepository {
   AuthRepositoryImpl(StorageRepository authInfoHolder) : _authInfoHolder = authInfoHolder {
     _dio = Dio(
       BaseOptions(
@@ -23,7 +27,12 @@ class AuthRepositoryImpl implements AuthRepository {
       authInfoInterceptor,
       errorExtractorInterceptor,
     ]);
+
+    fetchUserFromHive();
   }
+
+  @override
+  User? user;
 
   final StorageRepository _authInfoHolder;
   late final Dio _dio;
@@ -33,6 +42,20 @@ class AuthRepositoryImpl implements AuthRepository {
     box.put('id', user.id);
     box.put('email', user.email);
     box.put('imageUrl', user.imageUrl);
+  }
+
+  @override
+  Future<void> fetchUserFromHive() async {
+    try {
+      final box = await Hive.openBox('user');
+      user = User(
+        box.get('id'),
+        box.get('email'),
+        box.get('imageUrl') as String?,
+      );
+    } on Exception catch (e) {
+      user = null;
+    }
   }
 
   @override
@@ -49,11 +72,11 @@ class AuthRepositoryImpl implements AuthRepository {
 
     final json = response.data['user'];
 
-    final User user = User.fromJson(json);
+    user = User.fromJson(json);
 
-    await _storeUserToHive(user);
+    await _storeUserToHive(user!);
 
-    return user;
+    return user!;
   }
 
   @override
@@ -71,10 +94,58 @@ class AuthRepositoryImpl implements AuthRepository {
 
     final json = response.data['user'];
 
-    final User user = User.fromJson(json);
+    user = User.fromJson(json);
 
-    await _storeUserToHive(user);
+    await _storeUserToHive(user!);
 
-    return user;
+    return user!;
+  }
+
+  @override
+  Future<User> updateUser(String email, {File? imageFile}) async {
+    Response<dynamic> response;
+
+    if (imageFile != null) {
+      final String fileName = imageFile.path.split('/').last;
+      final formData = FormData.fromMap({
+        'email': email,
+        'image': await MultipartFile.fromFile(imageFile.path, filename: fileName),
+      });
+
+      response = await _dio.put(
+        '/users',
+        data: formData,
+      );
+    } else {
+      response = await _dio.put(
+        '/users',
+        data: {
+          'email': email,
+        },
+      );
+    }
+
+    _authInfoHolder.authInfo = AuthInfo.fromHeaderMap(response.headers.map);
+
+    final json = response.data['user'];
+
+    user = User.fromJson(json);
+
+    await _storeUserToHive(user!);
+
+    notifyListeners();
+
+    return user!;
+  }
+
+  @override
+  Future<void> logoutUser() async {
+    _deleteUserDataFromDevice();
+  }
+
+  Future<void> _deleteUserDataFromDevice() async {
+    await Hive.deleteBoxFromDisk('user');
+    _authInfoHolder.deleteUserData();
+    user = null;
   }
 }
