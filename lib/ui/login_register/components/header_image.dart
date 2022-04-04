cc1478ebf6d2c4e598739ba0792d49f78ae1f115
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-04 21:28:26 +0200

    Added animation for app logo

--- lib/ui/login_register/components/header_image.dart
+++ lib/ui/login_register/components/header_image.dart
@@ -22,13 +22,54 @@ class HeaderImage extends StatelessWidget {
             top: 0,
             right: 0,
           ),
-          Positioned(
-            child: Assets.images.logoHorizontalWhite.svg(),
+          const Positioned(
+            child: _AnimatedLogo(),
             left: 56.0,
             bottom: 50.0,
           ),
         ],
       ),
+    );
+  }
+}
+
+class _AnimatedLogo extends StatefulWidget {
+  const _AnimatedLogo({Key? key}) : super(key: key);
+
+  @override
+  State<_AnimatedLogo> createState() => _AnimatedLogoState();
+}
+
+class _AnimatedLogoState extends State<_AnimatedLogo> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(
+      seconds: 1,
+      milliseconds: 500,
+    ),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.0, 0.5),
+  ).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ),
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Assets.images.logoHorizontalWhite.svg(),
     );
   }
 }
