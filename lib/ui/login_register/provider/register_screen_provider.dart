99cf1c22e9a9f381a87f834ff06e47250f843135
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-09 20:59:00 +0200

    Removed form validation

--- lib/ui/login_register/provider/register_screen_provider.dart
+++ lib/ui/login_register/provider/register_screen_provider.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:tv_shows/common/utility/state/request_provider.dart';
 import 'package:tv_shows/source_remote/auth/auth_repository.dart';
 
@@ -11,32 +10,24 @@ class RegisterScreenProvider extends RequestProvider<User> {
 
   final AuthRepository _authRepository;
 
-  final formKey = GlobalKey<FormState>();
-
   String _email = '';
+
   String get email => _email;
 
   String _password = '';
 
-  bool _formValid = false;
-  bool get formValid => _formValid;
-
   void updateEmail(String email) {
     _email = email.trim();
-    _formValid = formKey.currentState?.validate() ?? false;
     notifyListeners();
   }
 
   void updatePassword(String password) {
     _password = password.trim();
-    _formValid = formKey.currentState?.validate() ?? false;
     notifyListeners();
   }
 
   Future<void> onRegisterPressed() async {
-    if (_formValid) {
-      await executeRequest(requestBuilder: () async => await _authRepository.registerUser(_email, _password));
-    }
+    await executeRequest(requestBuilder: () async => await _authRepository.registerUser(_email, _password));
     reset();
   }
 }
