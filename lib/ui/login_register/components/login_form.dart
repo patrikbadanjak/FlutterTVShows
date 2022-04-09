43b2d6c0f8b1d6403c8c9ad12f9b951cb71383c4
Patrik Badanjak <pbadanjak@gmail.com>
2022-04-09 20:57:56 +0200

    Converted to stateful and implemented form validation

--- lib/ui/login_register/components/login_form.dart
+++ lib/ui/login_register/components/login_form.dart
@@ -9,8 +9,23 @@ import 'package:tv_shows/ui/welcome_screen.dart';
 import '../../../common/utility/state/consumer_listener.dart';
 import 'general_dialog.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _formValid = false;
+
+  void _validateForm(BuildContext context) {
+    if (Form.of(context)?.validate() ?? false) {
+      setState(() => _formValid = true);
+    } else {
+      setState(() => _formValid = false);
+    }
+  }
 
   String? _validateEmail(String value) {
     if (value.isEmpty) {
@@ -50,80 +65,95 @@ class LoginForm extends StatelessWidget {
         LoginScreenProvider provider,
       ) {
         return Form(
-          key: provider.formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Login',
-                      style: GoogleFonts.roboto(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 34.0,
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    Text(
-                      'In order to continue, please log in',
-                      style: Theme.of(context).textTheme.bodyText2?.copyWith(color: Colors.white),
-                    ),
-                    const SizedBox(height: 26.0),
-                    TextFormField(
-                      autofocus: true,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(
-                        color: Colors.white,
-                      ),
-                      decoration: const InputDecoration(
-                        hintText: 'email@example.com',
-                        labelText: 'Email',
-                      ),
-                      textInputAction: TextInputAction.next,
-                      onChanged: (value) => provider.updateEmail(value),
-                      validator: (value) => _validateEmail(value ?? ''),
-                    ),
-                    const SizedBox(height: 24.0),
-                    PasswordInputField(
-                      labelText: 'Password',
-                      validated: false,
-                      onChanged: (value) => provider.updatePassword(value),
-                      onFieldSubmitted: (_) async => await provider.onLoginPressed(),
-                    ),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) => const RegisterScreen(),
-                            ),
-                          );
-                        },
-                        child: const Text(
-                          'Create account',
+          child: Builder(builder: (context) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 34.0,
                         ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 16.0),
+                      Text(
+                        'In order to continue, please log in',
+                        style: Theme.of(context).textTheme.bodyText2?.copyWith(color: Colors.white),
+                      ),
+                      const SizedBox(height: 26.0),
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: const InputDecoration(
+                          hintText: 'email@example.com',
+                          labelText: 'Email',
+                        ),
+                        textInputAction: TextInputAction.next,
+                        onChanged: (value) {
+                          provider.updateEmail(value);
+                          _validateForm(context);
+                        },
+                        validator: (value) => _validateEmail(value ?? ''),
+                      ),
+                      const SizedBox(height: 24.0),
+                      PasswordInputField(
+                        labelText: 'Password',
+                        validated: false,
+                        onChanged: (value) {
+                          _validateForm(context);
+                          provider.updatePassword(value);
+                        },
+                        onFieldSubmitted: (_) async {
+                          if (_formValid) {
+                            await provider.onLoginPressed();
+                          }
+                        },
+                      ),
+                      Center(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            'Create account',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              ElevatedButton.icon(
-                onPressed: () async => await provider.onLoginPressed(),
-                icon: provider.state.maybeWhen(
-                  orElse: () => Container(),
-                  loading: () => const CircularProgressIndicator(),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    if (_formValid) {
+                      await provider.onLoginPressed();
+                    }
+                  },
+                  icon: provider.state.maybeWhen(
+                    loading: () => const CircularProgressIndicator(),
+                    orElse: () => Container(),
+                  ),
+                  label: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    primary: _formValid ? Colors.white : Colors.white.withOpacity(0.4),
+                    onPrimary: _formValid ? Theme.of(context).primaryColor : Colors.white.withOpacity(0.7),
+                  ),
                 ),
-                label: const Text('Login'),
-                style: ElevatedButton.styleFrom(
-                  primary: provider.formValid ? Colors.white : Colors.white.withOpacity(0.4),
-                  onPrimary: provider.formValid ? Theme.of(context).primaryColor : Colors.white.withOpacity(0.7),
-                ),
-              ),
-            ],
-          ),
+              ],
+            );
+          }),
         );
       },
     );
