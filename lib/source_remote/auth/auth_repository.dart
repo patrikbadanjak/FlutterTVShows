28e2cb57f77af208e2c2be01981d1a8f8a8bdd70
Patrik Badanjak <pbadanjak@gmail.com>
2022-05-08 23:09:09 +0200

    Refactored to conform to SOLID principles

--- lib/source_remote/auth/auth_repository.dart
+++ lib/source_remote/auth/auth_repository.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:tv_shows/common/models/user.dart';
 
-abstract class AuthRepository with ChangeNotifier {
-  User? user;
-  Future<void> fetchUserFromHive();
-
+abstract class AuthRepository {
   Future<User> loginUser(String email, String password);
+
   Future<User> registerUser(String email, String password);
+
   Future<User> updateUser(String email, {File? imageFile});
+
   Future<void> logoutUser();
 }
